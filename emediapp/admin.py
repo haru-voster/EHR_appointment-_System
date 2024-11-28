29dcd5e719350e7de57d37329495bec07e1b0f60
haru-voster <harounbravo2@gmail.com>
2024-11-28 16:59:54 +0200

    EHR system

---
+++ emediapp/admin.py
@@ -0,0 +1,21 @@
+# admin site
+
+from django.contrib import admin
+from .models import Doctor,Patient,Appointment,PatientDischargeDetails
+# registering models
+class DoctorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Doctor, DoctorAdmin)
+
+class PatientAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Patient, PatientAdmin)
+
+class AppointmentAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Appointment, AppointmentAdmin)
+
+class PatientDischargeDetailsAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(PatientDischargeDetails, PatientDischargeDetailsAdmin)
+
