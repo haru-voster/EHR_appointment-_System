87a44cb04080cd5aa4d81f7c257eab38cf39c608
haru-voster <wabwireharoun2001@gmail.com>
2025-11-20 12:44:02 +0300

    previledges

--- emediapp/views.py
+++ emediapp/views.py
@@ -117,12 +117,8 @@ def patient_signup_view(request):
     return render(request, 'hospital/patientsignup.html', context=mydict)
 
 
-
-
-
-
 #-----------for checking user is doctor , patient or admin(by Haroun)
-#-----------for checking user is doctor, patient or admin (by Haroun)
+
 def is_admin(user):
     return user.is_superuser or user.groups.filter(name='ADMIN').exists()
 
