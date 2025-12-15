2ce6f827dd5e3214cbe56c6974b12a1842f3ea2a
haru-voster <wabwireharoun2001@gmail.com>
2025-12-15 22:44:26 +0300

    watermark

--- emediapp/watermark.py
+++ emediapp/watermark.py
@@ -4,7 +4,7 @@ from reportlab.lib.pagesizes import letter
 def create_watermark(file_path):
     c = canvas.Canvas(file_path, pagesize=letter)
     c.setFont("Helvetica", 20)
-    c.setFillGray(0.5, 0.5)  # Light gray text
+    c.setFillGray(0.5, 0.5)  
     c.drawString(100, 500, "EMEDI - Confidential")
     c.drawString(100, 480, "Date: 05/12/2024")
     c.save()
