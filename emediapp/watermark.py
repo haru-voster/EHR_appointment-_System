c02bbaf36e3ffdc590100ab88fd8898abc06d45b
haru-voster <wabwireharoun2001@gmail.com>
2026-01-04 22:37:43 +0300

    watermark

--- emediapp/watermark.py
+++ emediapp/watermark.py
@@ -5,7 +5,7 @@ def create_watermark(file_path):
     c = canvas.Canvas(file_path, pagesize=letter)
     c.setFont("Helvetica", 20)
     c.setFillGray(0.5, 0.5)  
-    c.drawString(100, 500, "EMEDI HR- Confidential")
+    c.drawString(100, 500, "EMEDI HR-Confidential")
     c.drawString(100, 480, "Date: 05/12/2024")
     c.save()
 
